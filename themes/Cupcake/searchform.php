6154adf53dd4a2eff21f2c99b24b6271218ada45
lishengzxc <eric@lishengcn.cn>
2015-05-11 18:44:05 +0800

    rm

--- themes/Cupcake/searchform.php
+++
@@ -1,3 +0,0 @@
-<form id="searchform" class="searchform" action="<?php bloginfo('url'); ?>" method="get" role="Search">
-	<input class="searchBox" id="s" type="text" name="s" autocomplete="off"></input>
-</form>
--- wp-content/themes/Cupcake/link.php
+++
@@ -1,8 +0,0 @@
-<?php
-	/*
-	Template Name: 友情連結
-	*/
-	get_header();
-?>
-<?php get_template_part('func/link'); ?>
-<?php get_footer(); ?>
